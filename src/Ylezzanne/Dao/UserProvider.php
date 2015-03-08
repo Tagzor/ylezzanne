085c89682ca9f4ee2d05c8cf8969fa2b24f1da1b
Ragmar Riismaa <riismaa@outlook.com>
2015-03-08 20:19:05 +0200

    pdo

--- src/Ylezzanne/Dao/UserProvider.php
+++ src/Ylezzanne/Dao/UserProvider.php
@@ -5,49 +5,39 @@ namespace Ylezzanne\Dao;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\User;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Herrera\Pdo\Pdo;
- 
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Doctrine\DBAL\Connection;
+
 class UserProvider implements UserProviderInterface
 {
-    /**
-	 *
-	 * @var \Herrera\Pdo\Pdo;
-	 *
-	 */
-	protected $pdo;
-	
-	
-	public function __construct(Pdo $pdo) {
-		$this->pdo = $pdo;
-	}
- 
+    private $conn;
+
+    public function __construct(Connection $conn)
+    {
+        $this->conn = $conn;
+    }
+
     public function loadUserByUsername($username)
     {
-    	$st = $pdo->prepare('SELECT u.*  FROM users u WHERE ( u.username = ? OR u.mail = ? );', array (	$username, 	$username) );
-    	$st->execute();
-    	
-    	$usersData = $st->fetchAll ();
-    	if (empty ( $usersData )) {
-    		throw new UsernameNotFoundException ( sprintf ( 'User "%s" not found.', $username ) );
-    	}
-    	//$user = $this->buildUser ( $usersData [0] );
-    	$user = new User($usersData [0]->getUsername(), $usersData [0]->getPassword(), explode(',', $user['roles']), true, true, true, true);
-    	return $user;
-    	
-        
+        $stmt = $this->conn->executeQuery('SELECT * FROM users WHERE username = ?', array(strtolower($username)));
+
+        if (!$user = $stmt->fetch()) {
+            throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
+        }
+
+        return new User($user['username'], $user['password'], explode(',', $user['roles']), true, true, true, true);
     }
- 
+
     public function refreshUser(UserInterface $user)
     {
         if (!$user instanceof User) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
         }
- 
+
         return $this->loadUserByUsername($user->getUsername());
     }
- 
+
     public function supportsClass($class)
     {
         return $class === 'Symfony\Component\Security\Core\User\User';
--- src/app.php
+++ src/app.php
@@ -48,8 +48,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
 // 				            'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
 // 				        ),
 						'users' => $app->share(function() use ($app) {
-							$pdo= $app['pdo'];
-							return new Ylezzanne\Dao\UserProvider($pdo);
+							return new Ylezzanne\Dao\UserProvider($app['pdo']);
 						}),
 				),
 		),
