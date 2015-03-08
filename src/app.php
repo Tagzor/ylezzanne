766e80f1c4d434994b42aa832824eb22aae10c27
Ragmar Riismaa <riismaa@outlook.com>
2015-03-08 17:26:14 +0200

    user admin
    admin/foo

--- src/app.php
+++ src/app.php
@@ -42,9 +42,9 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
 						'logout' => array(
 							'logout_path' => '/logout',
 						),
-						'users' => $app->share(function () use ($app) {
-							return new Ylezzanne\Dao\UserDao($app['pdo'], $app['security.encoder.digest']);
-						}),
+						'users' => array(
+				            'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+				        ),
 				),
 		),
 ));
