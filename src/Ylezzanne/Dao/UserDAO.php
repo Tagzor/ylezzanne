c6be8a8412db9648c2d13b05b852ed3542da300f
Ragmar Riismaa <riismaa@outlook.com>
2015-03-13 03:33:56 +0200

    asa

--- src/Ylezzanne/Dao/UserDAO.php
+++ src/Ylezzanne/Dao/UserDAO.php
@@ -64,7 +64,7 @@ class UserDAO implements UserProviderInterface {
  			$userData = $row;
 		}
 		
-		$user =  new User($usersData['username'], $usersData['password'], explode(',', $usersData['role']), true, true, true, true);
+		return new User($usersData['username'], $usersData['password'], explode(',', $usersData['role']), true, true, true, true);
 		
 	}
 
@@ -80,7 +80,7 @@ class UserDAO implements UserProviderInterface {
 			throw new UsernameNotFoundException ( sprintf ( 'User "%s" not found.', $username ) );
 		}
 		
-		$user =  new User($usersData['username'], $usersData['password'], explode(',', $usersData['role']), true, true, true, true);
+		return new User($usersData['username'], $usersData['password'], explode(',', $usersData['role']), true, true, true, true);
 		
 	}
 	
