62190b5ed64f5519f728f07c9feaa2cc36cfe1f0
Ragmar Riismaa <riismaa@outlook.com>
2015-03-17 23:20:37 +0200

    no id

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 0);
 error_reporting(E_ALL);// | E_STRICT
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../src/Auth/openid.ee-authentication.php';
+//require_once __DIR__.'/../src/Auth/openid.ee-authentication.php';
 
 $app = require __DIR__.'/../src/app.php';
 
