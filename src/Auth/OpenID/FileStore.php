ff4ec44df6d7baa5c96a5206dbea95c6ce6008f4
Ragmar Riismaa <riismaa@outlook.com>
2015-03-17 13:52:24 +0200

    interface.php

--- src/Auth/OpenID/FileStore.php
+++ src/Auth/OpenID/FileStore.php
@@ -20,7 +20,7 @@ namespace Auth\OpenID;
  * Require base class for creating a new interface.
  */
 use Auth\OpenID;
-require_once 'Auth/OpenID/Interface.php';
+require_once 'Interface.php';
 use Auth\OpenID\HMAC;
 use Auth\OpenID\Nonce;
 
