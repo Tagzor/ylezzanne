9a0fdfa38b67934c2eb53414525cbfa4f5649a78
Ragmar Riismaa <riismaa@outlook.com>
2015-03-22 17:51:53 +0200

    kordame

--- src/controllers.php
+++ src/controllers.php
@@ -15,120 +15,118 @@ $twig->get ( '/{name}', function ($name) use($app) {
 // define controllers for a user
 $user = $app ['controllers_factory'];
 $user->get ( '/{name}', function ($name) use($app) {
-	$stmt = $app ['pdo']->prepare("SELECT u.* FROM users u WHERE u.username = :name");
-	//$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-	$stmt->execute(array(':name' => $name));
+	$stmt = $app ['pdo']->prepare ( "SELECT u.* FROM users u WHERE u.username = :name" );
+	// $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+	$stmt->execute ( array (
+			':name' => $name 
+	) );
 	
-	$usersData = array();
+	$usersData = array ();
 	while ( $row = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
 		$app ['monolog']->addDebug ( 'Row ' . $row ['username'] );
 		$app ['monolog']->addDebug ( 'Row ' . $row ['password'] );
-	
-		$user =  new User($row['id'],$row['username'], $row['password'], explode(',', $row['role']), true, true, true, true);
-	
-		array_push($usersData, $user);
-	
-		$salt = uniqid(mt_rand());
-		$password = $app['security.encoder.digest']->encodePassword('foo', $salt);
-	
+		
+		$user = new User ( $row ['id'], $row ['username'], $row ['password'], explode ( ',', $row ['role'] ), true, true, true, true );
+		
+		array_push ( $usersData, $user );
+		
+		$salt = uniqid ( mt_rand () );
+		$password = $app ['security.encoder.digest']->encodePassword ( 'foo', $salt );
+		
 		$app ['monolog']->addDebug ( 'RowSalt ' . $salt );
 		$app ['monolog']->addDebug ( 'RowPAssword ' . $password );
-	
-	
-		print count($usersData );
+		
+		print count ( $usersData );
 	}
 	
 	if (empty ( $usersData )) {
 		echo "no user stored ";
 		return $app ['twig']->render ( 'user.twig', array (
-				'name' => 'no data stored name',
+				'name' => 'no data stored name' 
 		) );
-		
 	} else {
 		echo $usersData;
 	}
-	//PHP Catchable fatal error:  Object of class Ylezzanne\Dao\User could not be converted to string
-	//print join(",", $usersData);
-	$winner = $usersData[rand(0,count($usersData)-1)];
+	// PHP Catchable fatal error: Object of class Ylezzanne\Dao\User could not be converted to string
+	// print join(",", $usersData);
+	$winner = $usersData [rand ( 0, count ( $usersData ) - 1 )];
 	
 	return $app ['twig']->render ( 'user.twig', array (
-			'name' => $winner->getUsername(),
+			'name' => $winner->getUsername () 
 	) );
 } );
 
 $user->get ( '/', function () use($app) {
 	echo "load users! ";
 	
-	$st = $app ['pdo']->prepare('SELECT u.*  FROM users u');
-	$st->execute();
+	$st = $app ['pdo']->prepare ( 'SELECT u.*  FROM users u' );
+	$st->execute ();
 	
-	$usersData = array();
+	$usersData = array ();
 	while ( $row = $st->fetch ( PDO::FETCH_ASSOC ) ) {
 		$app ['monolog']->addDebug ( 'Row ' . $row ['username'] );
 		$app ['monolog']->addDebug ( 'Row ' . $row ['password'] );
 		
 		$user = buildUser ( $row );
 		
-		array_push($usersData, $user);
+		array_push ( $usersData, $user );
 		
-		$salt = uniqid(mt_rand());
-		$password = $app['security.encoder.digest']->encodePassword('foo', $salt);
+		$salt = uniqid ( mt_rand () );
+		$password = $app ['security.encoder.digest']->encodePassword ( 'foo', $salt );
 		
 		$app ['monolog']->addDebug ( 'RowSalt ' . $salt );
 		$app ['monolog']->addDebug ( 'RowPAssword ' . $password );
 		
-		
-        print count($usersData ); 
+		print count ( $usersData );
 	}
 	
 	if (empty ( $usersData )) {
 		echo "no user stored ";
 	} else {
-		echo $usersData;	
+		echo $usersData;
 	}
-	//PHP Catchable fatal error:  Object of class Ylezzanne\Dao\User could not be converted to string
-	//print join(",", $usersData);
-	$winner = $usersData[rand(0,count($usersData)-1)];
+	// PHP Catchable fatal error: Object of class Ylezzanne\Dao\User could not be converted to string
+	// print join(",", $usersData);
+	$winner = $usersData [rand ( 0, count ( $usersData ) - 1 )];
 	
 	return $app ['twig']->render ( 'user.twig', array (
-		'name' => $winner->getUsername(),
+			'name' => $winner->getUsername () 
 	) );
 } );
-	
-	
+
 // define controllers for a game
 $game = $app ['controllers_factory'];
 $game->get ( '/{id}', function ($id) use($app) {
-	$gameDAO = new Ylezzanne\Dao\GameDAO($app['pdo']);
-	$game = $gameDAO->find($id);
-	$topScores = $gameDAO->getTopScores($id);
-	$games = $gameDAO->findAll();
+	$gameDAO = new Ylezzanne\Dao\GameDAO ( $app ['pdo'] );
+	$game = $gameDAO->find ( $id );
+	$topScores = $gameDAO->getTopScores ( $id );
+	$games = $gameDAO->findAll ();
 	
 	return $app ['twig']->render ( 'game.twig', array (
 			'game' => $game,
 			'topScores' => $topScores,
-			'games' => $games
+			'games' => $games 
 	) );
 } );
 
 $game->get ( '/', function () use($app) {
-	$gameDAO = new Ylezzanne\Dao\GameDAO($app['pdo']);
-	$games = $gameDAO->findAll();
+	$gameDAO = new Ylezzanne\Dao\GameDAO ( $app ['pdo'] );
+	$games = $gameDAO->findAll ();
 	return $app ['twig']->render ( 'games.twig', array (
 			'games' => $games 
 	) );
 } );
-	
+
 // define controllers for a game statistics
 $statistics = $app ['controllers_factory'];
 $statistics->get ( '/game/{id}', function ($id) use($app) {
-	$gameDAO = new Ylezzanne\Dao\GameDAO($app['pdo']);
-	$games = $gameDAO->findAll();
+	$gameDAO = new Ylezzanne\Dao\GameDAO ( $app ['pdo'] );
+	$games = $gameDAO->findAll ();
 	
 	$token = $app ['security']->getToken ();
 	if (null !== $token) {
 		$user = $token->getUser ();
-		$stats = $gameDAO->getStatistics($id, $user->getUsername());
+		$stats = $gameDAO->getStatistics ( $id, $user->getUsername () );
 	}
 	
 	return $app ['twig']->render ( 'statistics.twig', array (
@@ -160,44 +158,41 @@ $app->get ( '/tbd/{name}', function ($name) use($app) {
 $app->get ( '/eid', function () use($app) {
 	
 	// Vaatame, mis muutujad olemas on ...
-	if(!empty($_SESSION)) {
+	if (! empty ( $_SESSION )) {
 		
 		echo "<h2>Siin kontroller \$_SESSION kuvab sisu:</h2>";
 		echo "<div class='alert'>";
-		foreach($_SESSION as $k => $v){
-			echo $k." = ".$v."<br>";
+		foreach ( $_SESSION as $k => $v ) {
+			echo $k . " = " . $v . "<br>";
 			
-			if (strcasecmp($k, 'openid') == 0) {
+			if (strcasecmp ( $k, 'openid' ) == 0) {
 				$openid = $v;
 			}
-			if (strcasecmp($k, 'email') == 0) {
+			if (strcasecmp ( $k, 'email' ) == 0) {
 				$email = $v;
 				$password = $v;
 			}
-			if (strcasecmp($k, 'fullname') == 0) {
+			if (strcasecmp ( $k, 'fullname' ) == 0) {
 				$fullname = $v;
 			}
 			
-			if (strcasecmp($k, 'gender') == 0) {
+			if (strcasecmp ( $k, 'gender' ) == 0) {
 				$gender = $v;
 			}
-			if (strcasecmp($k, 'dob') == 0) {
+			if (strcasecmp ( $k, 'dob' ) == 0) {
 				$dob = $v;
 			}
 		}
 		echo "</div>";
-		$userDAO = new Ylezzanne\Dao\UserDAO($app['pdo'], $app['security.encoder.digest']);
-		$user = new \Ylezzanne\Dao\User();
-		$user-> setUsername($email);
-		$user-> setPassword('ylezanne');
-		$user-> setMail($email);
+		$userDAO = new Ylezzanne\Dao\UserDAO ( $app ['pdo'], $app ['security.encoder.digest'] );
+		$user = new \Ylezzanne\Dao\User ();
+		$user->setUsername ( $email );
+		$user->setPassword ( 'ylezzanne' );
+		$user->setMail ( $email );
 		
-			$userDAO -> save($user);
-			$message = 'The user ' . $user->getUsername() . ' has been saved.';
-			$app['session']->getFlashBag()->add('success', $message);
-			// Redirect to the edit page.
-			//$redirect = $app['url_generator']->generate('user', array('user' => $user->getUsername()));
-			//return $app->redirect($redirect);
+		$userDAO->save ( $user );
+		$message = 'The user ' . $user->getUsername () . ' has been saved.';
+		$app ['session']->getFlashBag ()->add ( 'success', $message );
 	}
 	
 	return $app ['twig']->render ( 'eid.twig', array (
@@ -211,7 +206,7 @@ $app->get ( '/eid', function () use($app) {
 			'password' => $password,
 			'fullname' => $fullname,
 			'dob' => $dob,
-			'user'=> $user
+			'user' => $user 
 	) );
 } );
 
