a16cbc17c0eed04e209b31c18773400949c27466
Ragmar Riismaa <riismaa@outlook.com>
2015-04-26 22:40:17 +0300

    Unable to generate a URL for the named route

--- src/controllers.php
+++ src/controllers.php
@@ -63,7 +63,7 @@ $game->get ( '/{id}/{score}', function ($id, $score) use($app) {
 
 	$gameDAO->saveScore( $user->getId (), $game->getId (), $score);
 	
-	return $app->redirect($app['url_generator']->generate('db/game/'.$id));
+	return $app->redirect($app['url_generator']->generate('/db/game/'.$id));
 } );
 
 $game->get ( '/snake', function () use($app) {
